333070d2685ac916660e557aa087046403e7b4a6
Klizlo <klizlo0111@gmail.com>
2023-10-03 16:30:10 +0200

    Initial Commit

---
+++ ZPWS/ZPWSApp.swift
@@ -0,0 +1,17 @@
+//
+//  ZPWSApp.swift
+//  ZPWS
+//
+//  Created by student on 03/10/2023.
+//
+
+import SwiftUI
+
+@main
+struct ZPWSApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
